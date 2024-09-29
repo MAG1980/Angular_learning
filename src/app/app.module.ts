7b913043e86d3ba4fa33b72c1a4d9b911b3af74a
MAG1980 <al-1980@inbox.ru>
2024-09-29 14:09:31 +0300

    Создан ServerModule. Настроено использование в AppModule компонента, объявленного в ServerModule .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from "@angular/forms";
+import { ServerModule } from "./server/server.module";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { FormsModule } from "@angular/forms";
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ServerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/server/server.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { ServerComponent } from "./server.component";
+
+@NgModule({
+  declarations: [ServerComponent],
+  imports: [],
+  providers: [],
+  exports: [ServerComponent]
+})
+export class ServerModule {}
