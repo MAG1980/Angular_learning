25b868b66de97ef6e30a33bab3fbb804e9424de0
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2024-10-04 17:25:58 +0300

    ngDoCheck - метод жизненного цикла, который вызывается каждый раз, когда этот компонент проверяется на наличие изменений, например, при нажатии кнопки, резолве промиса и др.. В нём не следует выполнять сложную логику, т.к. это приведёт к снижению производительности. ngDoChek может быть использован для ручного изменения данных, если Angular не может выполнить эту операцию самостоятельно.

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, DoCheck, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrl: './server-element.component.css'
 })
-export class ServerElementComponent implements OnInit, OnChanges {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck {
   @Input("serverElement") element: { type: string, name: string, content: string }
   @Input("name") name = "";
 
@@ -19,5 +19,9 @@ export class ServerElementComponent implements OnInit, OnChanges {
 
   ngOnInit(): void {
     console.log('ngOnInit called');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck called');
   }
 }
