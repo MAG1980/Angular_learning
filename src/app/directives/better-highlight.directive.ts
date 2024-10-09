245a3bdcd9debb2cff67f7de13d3ad09ec26c9d2
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2024-10-09 21:02:05 +0300

    Реализована передача кастомных свойств в директиву с помощью параметров Property Binding (@Input(), @HostBinding(property)).

--- src/app/directives/better-highlight.directive.ts
+++ src/app/directives/better-highlight.directive.ts
@@ -1,10 +1,12 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent'
+  @Input() defaultColor = 'transparent'
+  @Input() highLightColor = 'blue'
+  @HostBinding('style.backgroundColor') backgroundColor: string
   @HostBinding('style.color') color: string = 'black'
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {
@@ -12,13 +14,14 @@ export class BetterHighlightDirective implements OnInit {
 
   ngOnInit() {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue')
+    this.backgroundColor = this.defaultColor
   }
 
   @HostListener('mouseenter') onMouseOver(event: Event) {
     console.log(event)
     /*this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue')
     this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'white')*/
-    this.backgroundColor = 'blue'
+    this.backgroundColor = this.highLightColor
     this.color = 'white'
   }
 
@@ -26,7 +29,7 @@ export class BetterHighlightDirective implements OnInit {
     console.log(event)
     /*    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent')
         this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'black')*/
-    this.backgroundColor = 'transparent'
+    this.backgroundColor = this.defaultColor
     this.color = 'black'
   }
 }
