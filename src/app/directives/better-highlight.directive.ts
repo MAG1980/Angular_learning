18c4ac14d214bcc799a90d0d5f4b130f4695d698
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2024-10-09 13:04:01 +0300

    Директива переписана с использованием декоратора @HostListener('property'), который позволяет привязать property DOM-элемента, к которому добавлена директива, к свойствам класса директивы, чтобы изменять их программно.

--- src/app/directives/better-highlight.directive.ts
+++ src/app/directives/better-highlight.directive.ts
@@ -1,9 +1,11 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent'
+  @HostBinding('style.color') color: string = 'black'
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {
   }
@@ -14,14 +16,18 @@ export class BetterHighlightDirective implements OnInit {
 
   @HostListener('mouseenter') onMouseOver(event: Event) {
     console.log(event)
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue')
-    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'white')
+    /*this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue')
+    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'white')*/
+    this.backgroundColor = 'blue'
+    this.color = 'white'
   }
 
   @HostListener('mouseleave') onMouseLeave(event: Event) {
     console.log(event)
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent')
-    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'black')
+    /*    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent')
+        this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'black')*/
+    this.backgroundColor = 'transparent'
+    this.color = 'black'
   }
 }
 
